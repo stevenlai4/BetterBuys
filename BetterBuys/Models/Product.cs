7ae98a084fa1295d586b4b52aa1ca72d047daac2
Wayne Lee <waynel1996weiyang@gmail.com>
2020-12-08 09:47:47 -0800

    Add model and view model for product.

---
+++ BetterBuys/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuys.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
