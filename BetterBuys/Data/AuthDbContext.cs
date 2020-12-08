1430bce99f8176eeffc6b34ca6fd6117d94da8f7
Steven <clai86@my.bcit.ca>
2020-12-07 16:20:31 -0800

    Rename AuthDbContext #24

--- BetterBuys/Data/AuthDbContext.cs
+++ BetterBuys/Data/AuthDbContext.cs
@@ -6,9 +6,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BetterBuys.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class AuthDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
             : base(options)
         {
         }
