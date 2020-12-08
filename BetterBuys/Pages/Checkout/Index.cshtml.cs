b3c77a5828734246f966f028d756e0fe13e0d30c
karenm08 <wmok14@my.bcit.ca>
2020-12-08 13:58:14 -0800

    created checkout page

---
+++ BetterBuys/Pages/Checkout/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BetterBuys.Pages.Checkout
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly StoreDbContext _db;
+        //public CheckoutModel(StoreDbContext db)
+        //{
+        //    _db = db;
+        //}
+        public void OnGet()
+        {
+        }
+    }
+}
