b149c81f05f6dcea1fcb475a40eb2355bbb579c0
karenm08 <wmok14@my.bcit.ca>
2020-12-09 13:56:37 -0800

    created ProductIndexVM

---
+++ BetterBuys/Interfaces/IProductVMService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuys.Interfaces
+{
+    public interface IProductVMService
+    {
+        ProductIndexVM GetProductsVM(int? typeId);
+        IEnumerable<SelectListItem> GetTypes();
+    }
+}
