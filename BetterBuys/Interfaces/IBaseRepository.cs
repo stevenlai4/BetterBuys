e96647beb1ffebec8ecc33062268eb89a0b6de34
karenm08 <wmok14@my.bcit.ca>
2020-12-09 11:15:34 -0800

    Added interface IBaseRepository

---
+++ BetterBuys/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using BetterBuys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuys.Interfaces
+{
+    interface IBaseRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+    }
+}
