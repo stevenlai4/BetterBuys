0cb59424e67decb76ffee04ba3206e36e24ec674
karenm08 <wmok14@my.bcit.ca>
2020-12-09 11:10:03 -0800

    created base repository

---
+++ BetterBuys/Data/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuys.Data
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly StoreDbContext _db;
+
+        public BaseRepository(StoreDbContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _db.Set<T>();
+        }
+    }
+}
