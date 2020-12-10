d309701c52e23e526ca6004ba1ac7ec33826b4a3
Kirill Kutnenko <kkutnenko@my.bcit.ca>
2020-12-10 12:19:16 -0800

    Initial commit #64

--- BetterBuys/Pages/Detail.cshtml.cs
+++ BetterBuys/Pages/Detail.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BetterBuys.Interfaces;
+using BetterBuys.Models;
+using BetterBuys.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,32 @@ namespace BetterBuys.Pages
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly IBaseRepository<Product> _productRepo;
+        public ProductVM Product = new ProductVM();
+
+        public DetailModel(IBaseRepository<Product> productRepo)
         {
+            _productRepo = productRepo;
+            
+        }
+        public void OnGet(int id)
+        {
+            IQueryable<Product> products = _productRepo.GetAll();
+            //Product = products.Where(p=>p.ProductId == id)
+            Product = products.Where(p => p.Id == id);
         }
     }
 }
+
+
+public ProductVMService(IBaseRepository<Product> productRepo, IBaseRepository<Category> categoryRepo)
+        {
+            _productRepo = productRepo;
+            _categoryRepo = categoryRepo;
+        }
+
+        public ProductIndexVM GetProductsVM(int? categoryId)
+        {
+            IQueryable<Product> products = _productRepo.GetAll();
+            if (categoryId != null)
+                products = products.Where(p => p.CategoryId == categoryId);
