fb6a15465d8dc8cd3d0ec39ef90c0ecb03d17898
Steven <clai86@my.bcit.ca>
2020-12-10 13:23:30 -0800

    GetProductsVM modified using LINQ expression

--- BetterBuys/Services/ProductVMService.cs
+++ BetterBuys/Services/ProductVMService.cs
@@ -1,7 +1,9 @@
-﻿using BetterBuys.Interfaces;
+﻿using BetterBuys.Data;
+using BetterBuys.Interfaces;
 using BetterBuys.Models;
 using BetterBuys.ViewModels;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,19 +14,28 @@ namespace BetterBuys.Services
     public class ProductVMService : IProductVMService
     {
         private readonly IBaseRepository<Product> _productRepo;
-        private readonly IBaseRepository<Category> _categoryRepo ;
+        private readonly IBaseRepository<Category> _categoryRepo;
+        private readonly StoreDbContext _db;
 
-        public ProductVMService(IBaseRepository<Product> productRepo, IBaseRepository<Category> categoryRepo)
+        public ProductVMService(IBaseRepository<Product> productRepo, IBaseRepository<Category> categoryRepo, StoreDbContext db)
         {
             _productRepo = productRepo;
             _categoryRepo = categoryRepo;
+            _db = db;
         }
 
         public ProductIndexVM GetProductsVM(int? categoryId)
         {
             IQueryable<Product> products = _productRepo.GetAll();
+            IQueryable<Category> categories = _categoryRepo.GetAll();
             if (categoryId != null)
-                products = products.Where(p => p.CategoryId == categoryId);
+            {
+                products = (from p in products
+                            join pc in _db.ProductCategories on p.Id equals pc.ProductId
+                            join c in categories on pc.CategoryId equals c.Id
+                            where c.Id == categoryId
+                            select p);
+            }
 
             var vm = new ProductIndexVM()
             {
@@ -43,12 +54,6 @@ namespace BetterBuys.Services
         public List<Category> GetCategories()
         {
             var categories = _categoryRepo.GetAll().Select(c => new Category(c.Name)).ToList();
-            //{
-            //    Value = c.Id.ToString(),
-            //    Text = c.Name
-            //}).OrderBy(t => t.Text).ToList();
-            //var allItem = new SelectListItem() { Value = null, Text = "All", Selected = true };
-            //categories.Insert(0, allItem);
             return categories;
         }
     }
