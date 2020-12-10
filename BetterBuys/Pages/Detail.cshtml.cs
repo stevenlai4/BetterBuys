ed9fed16f6f79fb6b19922eda00c794bcb6ac18c
abhishekpundir789 <apundir@algomau.ca>
2020-12-10 12:00:47 -0800

    update commit #66 with Details page

--- BetterBuys/Pages/Detail.cshtml.cs
+++ BetterBuys/Pages/Detail.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BetterBuys.Interfaces;
+using BetterBuys.Models;
+using BetterBuys.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,17 @@ namespace BetterBuys.Pages
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductVMService _productVMService;
+        public DetailModel(IProductVMService productVMService)
         {
+            _productVMService = productVMService;
+        }
+        public ProductIndexVM ProductIndex { get; set; } = new ProductIndexVM();
+        public List<Category> Categories { get; set; }
+        public void OnGet(ProductIndexVM productIndex)
+        {
+            ProductIndex = _productVMService.GetProductsVM(productIndex.TypesFilterApplied);
+            Categories = _productVMService.GetCategories();
         }
     }
 }
