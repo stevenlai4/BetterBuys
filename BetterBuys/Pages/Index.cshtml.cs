2926a053b26f34223776963b16ef77a83a6b5b37
abhishekpundir789 <apundir@algomau.ca>
2020-12-09 16:06:04 -0800

    update commit #14 with adding partials for navigation bar

--- BetterBuys/Pages/Index.cshtml.cs
+++ BetterBuys/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BetterBuys.Data;
+using BetterBuys.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,23 @@ namespace BetterBuys.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly StoreDbContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, StoreDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
-
+        public List<Category> Categories { get; set; }
         public void OnGet()
         {
+            Categories = new List<Category>
+            {
+                new Category("Clothes"),
+                new Category("Books"),
+                new Category("Electronics")
 
+            };
         }
     }
 }
