4371b55ed74e9d1de6c62fbdd74bd0167f3af467
karenm08 <wmok14@my.bcit.ca>
2020-12-14 13:04:56 -0800

    added asp-for to checkout form

--- BetterBuys/Models/CheckoutInfo.cs
+++ BetterBuys/Models/CheckoutInfo.cs
@@ -28,16 +28,16 @@ namespace BetterBuys.Models
         [Phone]
         [Required]
         public string Phone { get; set; }
-        [StringLength(12)]
+        //[StringLength(12)]
         [Required]
         public string CardNumber { get; set; }
-        [StringLength(100, MinimumLength = 10)]
+        //[StringLength(100, MinimumLength = 10)]
         [Required]
         public string CardHolderName { get; set; }
-        [StringLength(100, MinimumLength = 10)]
+        //[StringLength(100, MinimumLength = 10)]
         [Required]
         public string ExpirationDate { get; set; }
-        [StringLength(3)]
+        //[StringLength(3)]
         [Required]
         public string CVW { get; set; }
 
