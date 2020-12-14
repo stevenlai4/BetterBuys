1580e15028019e74f0e48bd55af2184d3b3db503
karenm08 <wmok14@my.bcit.ca>
2020-12-14 14:55:41 -0800

    added checkoutinfo VM

---
+++ BetterBuys/ViewModels/CheckoutInfoVM .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterBuys.ViewModels
+{
+    public class CheckoutInfoVM
+    {
+        public int CartId { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Apartment { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVW { get; set; }
+    }
+}
